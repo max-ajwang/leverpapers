296d40693561a97cc1c34f99df248b4e1ec1c9c7
max-ajwang <maxajwang@gmail.com>
2024-06-04 20:03:51 +0300

    Get All Orders

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+let orders = [
+  {
+    id: nanoid(),
+    order_type: 'writing',
+    pages: 3,
+    subject: 'coding',
+    topic: 'MERN stack',
+    sources: 3,
+  },
+  {
+    id: nanoid(),
+    order_type: 'editing',
+    pages: 3,
+    subject: 'social work',
+    topic: 'nursing',
+    sources: 3,
+  },
+];
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -17,6 +37,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
+});
+
+app.get('/api/v1/orders', (req, res) => {
+  res.status(200).json({ orders });
 });
 
 const port = process.env.PORT || 5100;
