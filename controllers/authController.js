cf18ff9f5df6c1bb9aa3eabdb1ad94fbc4813ad0
max-ajwang <maxajwang@gmail.com>
2024-06-10 01:49:00 +0300

    Hash Password and Hash Utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
 
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
+
 export const login = async (req, res) => {
   res.send('login');
 };
