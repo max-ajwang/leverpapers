0c78682f1e230ef45d702f8a795d67e042c15a4e
max-ajwang <maxajwang@gmail.com>
2024-06-10 09:38:29 +0300

    JWT, JWT ENV, httpOnly cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   // first registered user is an admin
@@ -22,5 +23,14 @@ export const login = async (req, res) => {
     user && (await comparePassword(req.body.password, user.password));
 
   if (!isUserValid) throw new UnauthenticatedError('invalid credentials');
-  res.send('login');
+
+  const token = createJWT({ userId: user._id, role: user.role });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged in' });
 };
