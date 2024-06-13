be196a5078746fdf9ae39e7c6dc2e7a83384c15a
max-ajwang <maxajwang@gmail.com>
2024-06-14 00:38:00 +0300

    Register User complete

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
 
-export const action = async (data) => {
-  console.log(data);
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/register', data);
+    return redirect('/login');
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
 
 const Register = () => {
