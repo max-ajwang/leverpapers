659475129ba230a57745f57edab82fb8e67ec78f
max-ajwang <maxajwang@gmail.com>
2024-06-14 07:21:37 +0300

    Register navigation state

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -17,6 +17,9 @@ export const action = async ({ request }) => {
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -33,8 +36,8 @@ const Register = () => {
         <FormRow type="email" name="email" defaultValue="maxajwang@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
 
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'isSubmitting' : 'submit'}
         </button>
 
         <p>
