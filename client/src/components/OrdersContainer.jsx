78a4c184c2810ef56d16a4a1770059aad5e36457
max-ajwang <maxajwang@gmail.com>
2024-06-14 17:44:07 +0300

    Orders container jsx

--- client/src/components/OrdersContainer.jsx
+++ client/src/components/OrdersContainer.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import Order from './Order';
+import Wrapper from '../assets/wrappers/OrdersContainer';
+import { useAllOrdersContext } from '../pages/AllOrders';
 
 const OrdersContainer = () => {
-  return <h2>OrdersContainer</h2>;
+  const { data } = useAllOrdersContext();
+  const { orders } = data;
+
+  if (orders.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No orders to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className="orders">
+        {orders.map((order) => {
+          return <Order key={order._id} {...order} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default OrdersContainer;
