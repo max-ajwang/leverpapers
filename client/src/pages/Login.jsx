5fbe36d0f4c73bf1d382238bf4306ec0397373ac
max-ajwang <maxajwang@gmail.com>
2024-10-07 15:16:57 +0300

    Removed Demo User

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,20 +20,6 @@ export const action = async ({ request }) => {
 const Login = () => {
   const navigate = useNavigate();
 
-  const loginDemoUser = async () => {
-    const data = {
-      email: 'test@test.com',
-      password: 'secret123',
-    };
-    try {
-      await customFetch.post('/auth/login', data);
-      toast.success('take a test drive');
-      navigate('/dashboard');
-    } catch (error) {
-      toast.error(error?.response?.data?.msg);
-    }
-  };
-
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -44,10 +30,6 @@ const Login = () => {
         <FormRow type="password" name="password" />
 
         <SubmitBtn />
-
-        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
-          explore the app
-        </button>
 
         <p>
           Not a member yet?
